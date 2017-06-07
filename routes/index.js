115f7683abf3544755da956739020881fe693240
star_linux <15910787956@189.cn>
2017-06-07 23:04:36 +0800

    实现主页分页移动端布局 以及ajax加载

---
+++ routes/index.js
@@ -0,0 +1,88 @@
+let https = require('https');
+
+Date.prototype.Format = function (fmt) { //author: meizz 
+    var o = {
+        "M+": this.getMonth() + 1, //月份 
+        "d+": this.getDate(), //日 
+        "h+": this.getHours(), //小时 
+        "m+": this.getMinutes(), //分 
+        "s+": this.getSeconds(), //秒 
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度 
+        "S": this.getMilliseconds() //毫秒 
+    };
+    if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+    if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+    return fmt;
+}
+
+function dealData(reData){
+  for(let i = 0;i< reData.length ; i++ ){
+      let temp = reData[i];
+      temp.re_create_at= new Date(reData[i].create_at).Format("yyyy-MM-dd hh:mm");
+      
+      if(temp.top){
+        temp.nature='置顶';
+        temp.class = 'top'
+        continue;
+      }
+      if(!temp.top&&temp.good){
+        temp.nature='精华';
+        temp.class = 'good'
+        continue;
+      }
+      if(temp.tab=='share'){
+        temp.nature = '分享';
+        temp.class = 'share'
+      }
+      else if(temp.tab=='ask'){
+        temp.nature = '问答';
+        temp.class = 'ask';
+      } 
+      else if(temp.tab=='job'){
+        temp.nature = '招聘';
+        temp.class = 'job';
+      } 
+  }
+  return reData;
+}
+
+module.exports = (app)=>{
+
+  app.get('/',(req,res)=>{
+    if(!req.query.tab) req.query.tab = '';
+    https.get('https://cnodejs.org/api/v1/topics?limit=15&mdrender=true&tab='+req.query.tab,(response)=>{
+      let data = null;
+      let html = '';
+      response.on('data',(chunk)=>{
+        response.setEncoding('utf8');
+        html+=chunk;
+      });
+      response.on('end',()=>{
+        data = JSON.parse(html);
+        if(data.success == true){
+          res.render('index',{topics:dealData(data.data),tab:req.query.tab});
+        }
+      });
+    });
+  });
+
+
+  app.get('/ajax',(req,res)=>{// /ajax?page=3&tab=share
+      https.get('https://cnodejs.org/api/v1/topics?limit=15&mdrender=true&tab='+req.query.tab+'&page='+req.query.page,(response)=>{
+      let data = null;
+      let html = '';
+      response.on('data',(chunk)=>{
+        response.setEncoding('utf8');
+        html+=chunk;
+      });
+      response.on('end',()=>{
+        data = JSON.parse(html);
+        if(data.success == true){
+          res.send(JSON.stringify(dealData(data.data)));
+        }
+      });
+    });
+
+  });
+}
