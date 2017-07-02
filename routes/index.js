073935804db0bceafb5c6bd2a22cb0bfa930b67f
star <15910787956@189.cn>
2017-07-03 00:02:25 +0800

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,26 @@ Date.prototype.Format = function (fmt) { //author: meizz
     return fmt;
 }
 
+Date.prototype.trans = function (lastTime) {
+  let fmtLastTime = new Date(lastTime).Format("yyyyMMddhhmm"); 
+  let nowTime = new Date().Format("yyyyMMddhhmm");
+  let year = parseInt(nowTime.substring(0,4)) - parseInt(fmtLastTime.substring(0,4));
+  let month = parseInt(nowTime.substring(4,6)) - parseInt(fmtLastTime.substring(4,6));
+  let day = parseInt(nowTime.substring(6,8)) - parseInt(fmtLastTime.substring(6,8));
+  let hour = parseInt(nowTime.substring(8,10)) - parseInt(fmtLastTime.substring(8,10));
+  let minute = parseInt(nowTime.substring(10,12)) - parseInt(fmtLastTime.substring(10,12));
+  if(year > 1 || year == 1 && month >= 0 ) return year+'年前';
+  if(year==1) return (12+month)+'月前';
+  if(month>1||month==1&&day>= 0) return month+'月前';
+  if(month==1) return (30+day)+'天前';
+  if(day >1 || day==1 && hour >= 0 ) return day+'天前';
+  if(day==1) return (24+hour)+'小时前';
+  if(hour>1||hour==1&&minute>=0) return hour+'小时前';
+  if(hour==1) return (60+minute)+'分钟前';
+  if(minute>=1) return minute+'分钟前';
+  return '刚刚';
+}
+
 function dealData(reData){
   for(let i = 0;i< reData.length ; i++ ){
       let temp = reData[i];
@@ -23,17 +43,17 @@ function dealData(reData){
       temp.re_create_at= new Date(reData[i].create_at).Format("yyyy-MM-dd hh:mm");
       if(temp.top){
         temp.nature='置顶';
-        temp.class = 'top'
+        temp.class = 'top';
         continue;
       }
       if(!temp.top&&temp.good){
         temp.nature='精华';
-        temp.class = 'good'
+        temp.class = 'good';
         continue;
       }
       if(temp.tab=='share'){
         temp.nature = '分享';
-        temp.class = 'share'
+        temp.class = 'share';
       }
       else if(temp.tab=='ask'){
         temp.nature = '问答';
@@ -50,6 +70,7 @@ function dealData(reData){
   }
   return reData;
 }
+
 
 module.exports = (app)=>{
   //主页分页请求
